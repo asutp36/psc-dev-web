aa3e8c198938aa4fec397af37a813c2b53850861
lexa <lexabor@mail.ru>
2019-08-16 00:23:13 +0300

    - сделал смену локализации через ресурсы;

---
+++ site/Inspinia_MVC5/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Inspinia_MVC5.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inspinia_MVC5.Controllers
+{
+    [Culture]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeCulture(string lang)
+        {
+            string returnUrl = Request.UrlReferrer.AbsolutePath;
+            List<string> cultures = new List<string>() { "ru", "en" };
+            if (!cultures.Contains(lang))
+                lang = "ru";
+
+            HttpCookie cookie = Request.Cookies["lang"];
+            if (cookie != null)
+                cookie.Value = lang;
+            else
+            {
+                cookie = new HttpCookie("lang");
+                cookie.HttpOnly = false;
+                cookie.Value = lang;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+
+            Response.Cookies.Add(cookie);
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ site/Inspinia_MVC5/Filters/CultureAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inspinia_MVC5.Filters
+{
+    public class CultureAttribute : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string cultureName = null;
+
+            HttpCookie cultureCookie = filterContext.HttpContext.Request.Cookies["lang"];
+            if (cultureCookie != null)
+                cultureName = cultureCookie.Value;
+            else
+                cultureName = "ru";
+
+            List<string> cultures = new List<string>() { "ru", "en"};
+            if (!cultures.Contains(cultureName))
+                cultureName = "ru";
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureName);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(cultureName);
+        }
+    }
+}
