5045d93631ef88eb15271f4434dbf91869e456b6
neckbor <neckbor@mail.ru>
2019-08-16 17:50:30 +0300

    подключил базу данных; запрос /api/sync/postop парсит и добавляет оперцию в таблицу;

--- webservice/WebService/WebService/Controllers/SyncController.cs
+++ webservice/WebService/WebService/Controllers/SyncController.cs
@@ -4,19 +4,23 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebService.Models;
 
 namespace WebService.Controllers
 {
     public class SyncController : ApiController
     {
+        private ModelDb _model = new Models.ModelDb();
 
         public string GetData()
         {
             return "sync";
         }
 
-        public void Post([FromBody]string body)
+        public void PostOp([FromBody]Operation operation)
         {
+            _model.Operations.Add(operation);
+            _model.SaveChanges();
             
         }
 
