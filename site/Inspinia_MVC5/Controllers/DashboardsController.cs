bbcef09a68468c074afc7a1b87fbccea8c9fd6f7
mymailvrn98 <mymailvrn98@gmail.com>
2019-08-20 15:24:15 +0300

    решаю проблему с отображением

--- site/Inspinia_MVC5/Controllers/DashboardsController.cs
+++ site/Inspinia_MVC5/Controllers/DashboardsController.cs
@@ -10,14 +10,10 @@ namespace Inspinia_MVC5.Controllers
 {
     public class DashboardsController : HomeController
     {
-        static ModelDb db = new ModelDb();
+        static ContextDb db = new ContextDb();
 
         IEnumerable<Card> cards = db.Cards;
         IEnumerable<Operation> operations = db.Operations;
-
-        static ModelOfIncomes dbadd = new ModelOfIncomes();
-        
-        IEnumerable<Income> incomes = dbadd.Incomes;
 
         public ActionResult Dashboard_1()
         {
@@ -29,7 +25,6 @@ namespace Inspinia_MVC5.Controllers
         {
             ViewBag.Cards = cards;
             ViewBag.Operations = operations;
-            ViewBag.Incomes = incomes;
 
             return View();
         }
