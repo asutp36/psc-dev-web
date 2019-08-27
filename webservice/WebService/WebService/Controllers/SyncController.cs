9755c9becf7612b7308852c7aac762000e41b4c6
neckbor <neckbor@mail.ru>
2019-08-27 21:20:16 +0300

    закоментил работу с бд; добавил тестовый гет-запрос;

--- webservice/WebService/WebService/Controllers/SyncController.cs
+++ webservice/WebService/WebService/Controllers/SyncController.cs
@@ -8,6 +8,9 @@ using WebService.Models;
 
 namespace WebService.Controllers
 {
+    public class Coordinate { public int x { get; set; } public int y { get; set; } }
+
+
     public class SyncController : ApiController
     {
         private ModelDb _model = new Models.ModelDb();
@@ -23,8 +26,8 @@ namespace WebService.Controllers
         {
             if (operation != null)
             {
-                _model.Operations.Add(operation);
-                _model.SaveChanges();
+                //_model.Operations.Add(operation);
+                //_model.SaveChanges();
 
                 var responseGood = Request.CreateResponse(HttpStatusCode.OK);
                 responseGood.Headers.Add("ServerID", "1488");
@@ -43,8 +46,8 @@ namespace WebService.Controllers
         {
             if (owner != null)
             {
-                _model.Owners.Add(owner);
-                _model.SaveChanges();
+                //_model.Owners.Add(owner);
+                //_model.SaveChanges();
 
                 var responseGood = Request.CreateResponse(HttpStatusCode.OK);
                 responseGood.Headers.Add("ServerID", "1488");
@@ -64,8 +67,8 @@ namespace WebService.Controllers
         {
             if (card != null)
             {
-                _model.Cards.Add(card);
-                _model.SaveChanges();
+                //_model.Cards.Add(card);
+                //_model.SaveChanges();
 
                 var responseGood = Request.CreateResponse(HttpStatusCode.OK);
                 responseGood.Headers.Add("ServerID", "1488");
@@ -76,6 +79,15 @@ namespace WebService.Controllers
             responseBad.Headers.Add("ServerID", "1488");
 
             return responseBad;
+        }
+
+        [HttpGet]
+        [ActionName("test")]
+        public string Test(int x, int y)
+        {
+            int x1 = x * 2;
+            //int y1 = int.Parse(y) * 2;
+            return "x1=" + x1.ToString() + "y1=" + y.ToString();
         }
 
     }
