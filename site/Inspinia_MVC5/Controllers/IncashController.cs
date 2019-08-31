589c462dfde103df7eb7666aec429a7b5de20d4e
lexa <lexabor@mail.ru>
2019-09-01 01:26:12 +0300

    - зачисления datatable из базы;

---
+++ site/Inspinia_MVC5/Controllers/IncashController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Inspinia_MVC5.Models;
+
+namespace Inspinia_MVC5.Controllers
+{
+    public class IncashController : Controller
+    {
+        private ModelDb db = new ModelDb();
+
+        public ActionResult ProductsGrid()
+        {
+            return View();
+        }
+
+        public ActionResult IncashWashes()
+        {
+            DateTime startDTime = new DateTime(2019, 6, 20, 0, 0, 0);
+            DateTime stopDTime = new DateTime(2019, 6, 21, 0, 0, 0);
+
+            ViewBag.Incomes = GetIncomesFromDB(startDTime, stopDTime, "", "");
+
+            return View();
+        }
+
+        private List<GetWashAmounts_Result> GetIncomesFromDB(DateTime begTime, DateTime endTime, string region, string wash)
+        {
+            List<GetWashAmounts_Result> resultset = null;
+
+            //            DateTime dtEnd = DateTime.Today;
+            //            DateTime dtBeg = dtEnd.AddDays(-1);
+
+            var prmRegionCode = new System.Data.SqlClient.SqlParameter("@p_RegionCode", System.Data.SqlDbType.Int);
+            prmRegionCode.Value = 0;
+
+            Int32 intval;
+            if (Int32.TryParse(region, out intval))
+                prmRegionCode.Value = intval;
+
+            var prmWashCode = new System.Data.SqlClient.SqlParameter("@p_WashCode", System.Data.SqlDbType.NVarChar);
+            prmWashCode.Value = wash;
+
+            var prmPostCode = new System.Data.SqlClient.SqlParameter("@p_PostCode", System.Data.SqlDbType.NVarChar);
+            prmPostCode.Value = DBNull.Value;
+
+            var prmBeg = new System.Data.SqlClient.SqlParameter("@p_DateBeg", System.Data.SqlDbType.DateTime);
+            prmBeg.Value = begTime;
+
+            var prmEnd = new System.Data.SqlClient.SqlParameter("@p_DateEnd", System.Data.SqlDbType.DateTime);
+            prmEnd.Value = endTime;
+
+            var result = db.Database
+                .SqlQuery<GetWashAmounts_Result>("GetWashAmounts @p_RegionCode, @p_WashCode, @p_PostCode, @p_DateBeg, @p_DateEnd", prmRegionCode, prmWashCode, prmPostCode, prmBeg, prmEnd)
+                .ToList();
+
+            resultset = result;
+
+            return resultset;
+        }
+    }
+}
