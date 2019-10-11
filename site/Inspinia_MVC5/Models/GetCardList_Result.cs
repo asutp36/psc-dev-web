98fd3861ac873687aa8404bb5760b29d85e91df8
mymailvrn98 <mymailvrn98@gmail.com>
2019-10-11 18:51:56 +0300

    подключил БД с хранимкой GetCardsList;

--- site/Inspinia_MVC5/Models/GetCardList_Result.cs
+++ site/Inspinia_MVC5/Models/GetCardList_Result.cs
@@ -10,12 +10,20 @@
 namespace Inspinia_MVC5.Models
 {
     using System;
-    using System.Collections.Generic;
     
-    public partial class vPostsAmount
+    public partial class GetCardList_Result
     {
-        public int IDPost { get; set; }
-        public System.DateTime DTime { get; set; }
-        public Nullable<int> amount { get; set; }
+        public string Phone { get; set; }
+        public string CardNum { get; set; }
+        public string CardTypeName { get; set; }
+        public string CardStatusName { get; set; }
+        public int Balance { get; set; }
+        public System.DateTime ActivationDate { get; set; }
+        public int ActivationBy { get; set; }
+        public System.DateTime LastOperationDate { get; set; }
+        public int LastOperationBy { get; set; }
+        public int IncreaseSum { get; set; }
+        public int DecreaseSum { get; set; }
+        public Nullable<int> CountOperation { get; set; }
     }
 }
--- site/Inspinia_MVC5/Models/sysdiagram.cs
+++ site/Inspinia_MVC5/Models/sysdiagram.cs
@@ -12,12 +12,12 @@ namespace Inspinia_MVC5.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class User
+    public partial class sysdiagram
     {
-        public int IDUser { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-        public string Description { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
