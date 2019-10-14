4d66b644811f4550615fea6b46816af9719d8cd6
neckbor <neckbor@mail.ru>
2019-10-14 13:23:19 +0300

    разобрал xls;

--- TryParseReports/TryParseReports/Program.cs
+++ TryParseReports/TryParseReports/Program.cs
@@ -17,10 +17,17 @@ namespace TryParseReports
 
             object[,] data = worksheet.UsedRange.Value;
 
-            for (int r = 3; r < data.GetLength(0); r++)
+            for (int r = 3; r < 10; r++)
             {
-                for (int c = 1; c <= data.GetLength(1); c++)
-                    Console.Write(data[r, c].ToString() + ' ');
+                DateTime startDate = DateTime.Parse(data[r, 1].ToString());
+                DateTime stopDate = DateTime.Parse(data[r, 2].ToString());
+                DateTime vupdatetime2 = DateTime.Parse(data[r, 9].ToString());
+                Console.WriteLine("INSERT INTO %table% (startDate, stopDate, regionid, regionname, washname, wshaddress, washnum, postname, vupdatetime2, " +
+                    "msgtotal, b10, b50, b100, b500, b1k, m10, s, linkcontext)" +
+                                            $" VALUES({startDate.ToString("yyyyMMdd")}, " +
+                                            $"{stopDate.ToString("yyyyMMdd")}, {data[r, 3]}, \'{data[r, 4]}\', {data[r, 5]}, {data[r, 6]}, {data[r, 7]}, {data[r, 8]}, " +
+                                            $"{vupdatetime2.ToString("yyyyMMdd HH:mm:ss.fff")}, {data[r, 10]}, {data[r, 11]}, {data[r, 12]}, {data[r, 13]}, {data[r, 14]}, {data[r, 15]}, {data[r, 16]}, {data[r, 17]}, {data[r, 18]});" +
+                                            " SELECT SCOPE_IDENTITY()");
                 Console.WriteLine();
             }
 
