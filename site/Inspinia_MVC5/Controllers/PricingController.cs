bfff28e9f74e9a65ea02e808449b912bec054f89
mymailvrn98 <mymailvrn98@gmail.com>
2020-01-09 16:34:12 +0300

    создал страницу для изменения цен и переход на ней с главной страницы;

---
+++ site/Inspinia_MVC5/Controllers/PricingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inspinia_MVC5.Controllers
+{
+    public class PricingController : Controller
+    {
+        public ActionResult NewPricesView()
+        {
+            return View();
+        }
+    }
+}
