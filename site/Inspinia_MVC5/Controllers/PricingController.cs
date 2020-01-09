bc6c6e65120fb219ac1e26f96ee03b342faebcf5
mymailvrn98 <mymailvrn98@gmail.com>
2020-01-09 22:35:26 +0300

    сделал древовидный список;
    так же рядом с ним сделал  8 полей ввода;

--- site/Inspinia_MVC5/Controllers/PricingController.cs
+++ site/Inspinia_MVC5/Controllers/PricingController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Inspinia_MVC5.Models;
 
 namespace Inspinia_MVC5.Controllers
 {
     public class PricingController : Controller
     {
+        private ModelDb db = new ModelDb();
+
+        List<Region> _regions = null;
+
+        public PricingController()
+        {
+            _regions = db.Regions.ToList();
+        }
+
         public ActionResult NewPricesView()
         {
-            return View();
+            return View(_regions);
         }
     }
 }
