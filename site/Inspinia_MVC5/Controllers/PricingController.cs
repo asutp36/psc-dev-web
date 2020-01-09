8ba60e2ad552712a74d8048488108ed59f0bb1af
mymailvrn98 <mymailvrn98@gmail.com>
2020-01-09 23:57:07 +0300

    сделал взятие данных от пользователя;
    пытаюсь реализовать обновление страницы после вызова метода;

--- site/Inspinia_MVC5/Controllers/PricingController.cs
+++ site/Inspinia_MVC5/Controllers/PricingController.cs
@@ -18,6 +18,23 @@ namespace Inspinia_MVC5.Controllers
             _regions = db.Regions.ToList();
         }
 
+        public void SaveNewPrices(int pr1, int pr2, int pr3, int pr4, int pr5, int pr6, int pr7, int pr8, List<int> posts)
+        {
+            List<int> prices = new List<int>();
+
+            prices.Add(pr1);
+            prices.Add(pr2);
+            prices.Add(pr3);
+            prices.Add(pr4);
+            prices.Add(pr5);
+            prices.Add(pr6);
+            prices.Add(pr7);
+            prices.Add(pr8);
+
+            //отправка запроса на изменение цен на каждом посте
+            //return View("NewPricesView", _regions);
+        }
+
         public ActionResult NewPricesView()
         {
             return View(_regions);
