cf79d7e6bd55ed0c23141c40932b3e4afd4f1425
mymailvrn98 <mymailvrn98@gmail.com>
2020-01-16 22:25:51 +0300

    создал страницу для мониторинга постов;
    сделал возможность выбора поста;
    на той же странице отображение поста;
    тестирую на времени,которое просто вызываю DateTime.Now;
    не могу решить проблему с удалением прошлого выбранного поста;

---
+++ site/Inspinia_MVC5/Controllers/MonitoringController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inspinia_MVC5.Models;
+
+namespace Inspinia_MVC5.Controllers
+{
+    public class MonitoringController : Controller
+    {
+        private ModelDb db = new ModelDb();
+
+        List<Company> _companies = null;
+        List<Post> _posts = null;
+
+        Post post = null;
+
+        public MonitoringController()
+        {
+            _posts = db.Posts.ToList();
+            _companies = db.Companies.ToList();
+        }
+
+        public ActionResult MonitoringView()
+        {
+            return View(_companies);
+        }
+
+        public ActionResult PostMonitoringView(int IDPost)
+        {
+            post = _posts.Find(x => x.IDPost == IDPost);
+
+            return PartialView("PostMonitoringView", post);
+        }
+    }
+}
