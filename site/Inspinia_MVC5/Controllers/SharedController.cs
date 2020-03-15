b3d7d0a5924951b59675ee4f25be484194a3e29d
mymailvrn98 <mymailvrn98@gmail.com>
2020-03-15 23:46:51 +0300

    изменил иконку Ивана на ECO;
    попытался реализовать перенос постов в меню;

---
+++ site/Inspinia_MVC5/Controllers/SharedController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inspinia_MVC5.Models;
+
+namespace Inspinia_MVC5.Views.Shared
+{
+    public class SharedController : Controller
+    {
+        private ModelDb db = new ModelDb();
+
+        List<Region> _regions = null;
+
+        public SharedController()
+        {
+            _regions = db.Regions.ToList();
+        }
+
+        public ActionResult _Navigation()
+        {
+            return PartialView(_regions);
+        }
+    }
+}
