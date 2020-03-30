4bbc8d452aae7ac63355395959cb3c31e2d54371
neckbor <neckbor@mail.ru>
2020-03-30 16:07:37 +0300

    - добавил проект; - прописал класс отправки запроса на сервер WhattsAppSender;

---
+++ service/ReportNotificationWhattsapp/ReportNotificationWhattsapp/Supplies/WhattsAppSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportNotificationWhattsapp.Supplies
+{
+    class WhattsAppSender
+    {
+        public static ResponseSendMessage SendMessage(string json, string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.KeepAlive = false;
+            request.ProtocolVersion = HttpVersion.Version10;
+            request.Method = "POST";
+
+            byte[] postBytes = Encoding.UTF8.GetBytes(json);
+
+            request.ContentType = "application/json; charset=UTF-8";
+            request.Accept = "application/json";
+            request.ContentLength = postBytes.Length;
+
+            Stream requestStream = request.GetRequestStream();
+
+            requestStream.Write(postBytes, 0, postBytes.Length);
+            requestStream.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string result;
+            using (StreamReader rdr = new StreamReader(response.GetResponseStream()))
+            {
+                result = rdr.ReadToEnd();
+            }
+
+            return JsonConvert.DeserializeObject<ResponseSendMessage>(result);
+        }
+    }
+}
