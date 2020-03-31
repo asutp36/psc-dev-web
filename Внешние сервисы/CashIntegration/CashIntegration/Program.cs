5e5e9b84cddabd835eb4a7e5516b0465de838610
neckbor <neckbor@mail.ru>
2020-03-31 12:01:10 +0300

    - добавил логгер; - изменён запрос к базе; - добавил обработки ошибок и проверки;

--- Внешние сервисы/CashIntegration/CashIntegration/Program.cs
+++ Внешние сервисы/CashIntegration/CashIntegration/Program.cs
@@ -94,91 +94,111 @@ namespace CashIntegration
 
 
             List<CashWId> cashes = GetDataToSend();
-            DbConnection con = model.Database.Connection;
-            con.Open();
-
-            DbCommand comm = con.CreateCommand();
-
-
-            foreach (CashWId c in cashes)
+            if (cashes != null)
             {
-                Cash cash = new Cash();
-                cash.Date = c.Date;
-                cash.CashIncome = c.CashIncome;
-                cash.Code = c.Code;
-                
-                string data = JsonConvert.SerializeObject(c);
-                IntegrationResponse response = HttpSender.SendCash("https://api.myeco24.ru/transactions/post/cash", data, true);
-                //IntegrationResponse response = new IntegrationResponse();
-                //response.StatusCode = 200;
-                //response.TransactionId = 88;
 
-                comm.CommandText = $"INSERT INTO CashIntegration (IDEvent, ServerID, ServerMessage, ServerStatus) " +
-                $"VALUES ({c.ID}, {response.TransactionId}, '{response.Message}', {response.StatusCode})";
-                comm.ExecuteScalar();
+
+                DbConnection con = model.Database.Connection;
+                con.Open();
+
+                DbCommand comm = con.CreateCommand();
+
+
+                foreach (CashWId c in cashes)
+                {
+                    Cash cash = new Cash();
+                    cash.Date = c.Date;
+                    cash.CashIncome = c.CashIncome;
+                    cash.Code = c.Code;
+
+                    string data = JsonConvert.SerializeObject(c);
+                    IntegrationResponse response = HttpSender.SendCash("https://api.myeco24.ru/transactions/post/cash", data, true);
+                    Console.WriteLine(response.StatusCode);
+                    //IntegrationResponse response = new IntegrationResponse();
+                    //response.StatusCode = 200;
+                    //response.TransactionId = 88;
+
+                    comm.CommandText = $"INSERT INTO CashIntegration (IDEvent, ServerID, ServerMessage, ServerStatus) " +
+                    $"VALUES ({c.ID}, {response.TransactionId}, '{response.Message}', {response.StatusCode})";
+                    comm.ExecuteScalar();
+                }
+                con.Close();
             }
-            con.Close();
 
         }
 
         public static List<CashWId> GetDataToSend()
         {
-            List<CashWId> cash = new List<CashWId>();
-            
+            Logger.InitLogger();
+
             ModelDb model = new ModelDb();
 
-            DbConnection con1 = model.Database.Connection;
-            con1.Open();
-            DbCommand command = con1.CreateCommand();
-
-            command.CommandText = "select" +
-                " * " +
-                "from " +
-                "(" +
-                "select" +
-                " e.IDEvent, p.Code, e.DTime, ei.amount " +
-                "from EventIncrease ei " +
-                "join Event e on e.IDEvent = ei.IDEvent " +
-                "join Posts p on e.IDPost = p.IDPost " +
-                ") t" +
-                " left join CashIntegration ci on ci.IDEvent = t.IDEvent " +
-                "where 1=1" +
-                " and ci.IDEvent is NULL " +
-                "and t.DTime < GETDATE() - 1";
-            DbDataReader reader = command.ExecuteReader();
-
-            while(reader.Read())
+            try
             {
-                
-                CashWId c = new CashWId();
-                c.Code = reader.GetString(reader.GetOrdinal("Code"));
-                c.Date = reader.GetDateTime(reader.GetOrdinal("DTime"));
-                int amount = reader.GetInt32(reader.GetOrdinal("Amount"));
-                c.ID = reader.GetInt32(reader.GetOrdinal("IDEvent"));
+                List<CashWId> cash = new List<CashWId>();
 
-                switch (amount)
-                {
-                    case 10:
-                        c.CashIncome.Add(new CashIncome("m10", 1));
-                        break;
-                    case 50:
-                        c.CashIncome.Add(new CashIncome("b50", 1));
-                        break;
-                    case 100:
-                        c.CashIncome.Add(new CashIncome("b100", 1));
-                        break;
-                    case 200:
-                        c.CashIncome.Add(new CashIncome("b200", 1));
-                        break;
-                }
+                DbConnection con1 = model.Database.Connection;
+                con1.Open();
+                Logger.Log.Debug("Подключение к БД: " + con1.State);
 
-                cash.Add(c);
+                DbCommand command = con1.CreateCommand();
+
+                command.CommandText = "select" +
+                    " * " +
+                    "from " +
+                    "(" +
+                    "select" +
+                    " e.IDEvent, p.Code, e.DTime, ei.amount " +
+                    "from EventIncrease ei " +
+                    "join Event e on e.IDEvent = ei.IDEvent " +
+                    "join Posts p on e.IDPost = p.IDPost " +
+                    ") t" +
+                    " left join CashIntegration ci on ci.IDEvent = t.IDEvent " +
+                    "where 1=1" +
+                    " and ci.IDEvent is NULL " +
+                    "and t.DTime < CAST(GETDATE() AS date)";
+                DbDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    while (reader.Read())
+                    {
+
+                        CashWId c = new CashWId();
+                        c.Code = reader.GetString(reader.GetOrdinal("Code"));
+                        c.Date = reader.GetDateTime(reader.GetOrdinal("DTime"));
+                        int amount = reader.GetInt32(reader.GetOrdinal("Amount"));
+                        c.ID = reader.GetInt32(reader.GetOrdinal("IDEvent"));
+
+                        switch (amount)
+                        {
+                            case 10:
+                                c.CashIncome.Add(new CashIncome("m10", 1));
+                                break;
+                            case 50:
+                                c.CashIncome.Add(new CashIncome("b50", 1));
+                                break;
+                            case 100:
+                                c.CashIncome.Add(new CashIncome("b100", 1));
+                                break;
+                            case 200:
+                                c.CashIncome.Add(new CashIncome("b200", 1));
+                                break;
+                        }
+
+                        cash.Add(c);
+                    }
+
+                Logger.Log.Debug("Считано строк: " + cash.Count + Environment.NewLine);
+                reader.Close();
+                con1.Close();
+                return cash;
             }
-
-           
-            reader.Close();
-            con1.Close();
-            return cash;
+            catch(Exception e)
+            {
+                Logger.Log.Error("Ошибка: " + Environment.NewLine + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+                model.Database.Connection.Close();
+                return null;
+            }
         }
     }
 }
