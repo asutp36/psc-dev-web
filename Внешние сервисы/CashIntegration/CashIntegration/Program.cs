ee3370cb6d53e54aa6b86831af230532783bd05b
neckbor <neckbor@mail.ru>
2020-04-01 20:02:08 +0300

    - программа работает бесконечно; - добавил вывод времени последних синхронизированных записей с постов;

--- Внешние сервисы/CashIntegration/CashIntegration/Program.cs
+++ Внешние сервисы/CashIntegration/CashIntegration/Program.cs
@@ -15,114 +15,54 @@ namespace CashIntegration
     {
         static void Main(string[] args)
         {
+            Logger.InitLogger();
+
             ModelDb model = new ModelDb();
+            List<CashWId> cashes = new List<CashWId>();
 
-            #region предыдущая версия
-            //while (true)
-            //{
-            //    DbConnection con1 = model.Database.Connection;
-            //    con1.Open();
-            //    DbCommand command = con1.CreateCommand();
-
-            //    command.CommandText = "select" +
-            //        " * " +
-            //        "from " +
-            //        "(" +
-            //        "select" +
-            //        " e.IDEvent, p.Code, e.DTime, ei.amount " +
-            //        "from EventIncrease ei " +
-            //        "join Event e on e.IDEvent = ei.IDEvent " +
-            //        "join Posts p on e.IDPost = p.IDPost " +
-            //        ") t" +
-            //        " left join CashIntegration ci on ci.IDEvent = t.IDEvent " +
-            //        "where 1=1" +
-            //        " and ci.IDEvent is NULL " +
-            //        "and t.DTime < GETDATE() - 1";
-
-            //    DbDataReader reader = command.ExecuteReader();
-
-            //    if (!reader.HasRows)
-            //    {
-            //        reader.Close();
-            //        con1.Close();
-            //        break;
-            //    }
-
-            //    reader.Read();
-
-            //    Cash c = new Cash();
-            //    c.Code = reader.GetString(reader.GetOrdinal("Code"));
-            //    c.Date = reader.GetDateTime(reader.GetOrdinal("DTime"));
-            //    int amount = reader.GetInt32(reader.GetOrdinal("Amount"));
-            //    int id = reader.GetInt32(reader.GetOrdinal("IDEvent"));
-
-            //    switch (amount)
-            //    {
-            //        case 10:
-            //            c.CashIncome.Add(new CashIncome("m10", 1));
-            //            break;
-            //        case 50:
-            //            c.CashIncome.Add(new CashIncome("b50", 1));
-            //            break;
-            //        case 100:
-            //            c.CashIncome.Add(new CashIncome("b100", 1));
-            //            break;
-            //        case 200:
-            //            c.CashIncome.Add(new CashIncome("b200", 1));
-            //            break;
-            //    }
-            //    reader.Close();
-            //    con1.Close();
-            //    string data = JsonConvert.SerializeObject(c);
-            //    IntegrationResponse response = HttpSender.SendCash("https://api.myeco24.ru/transactions/post/cash", data, true);
-            //    //IntegrationResponse response = new IntegrationResponse();
-            //    //response.StatusCode = 200;
-            //    //response.TransactionId = 88;
-
-            //    DbConnection con2 = model.Database.Connection;
-            //    con2.Open();
-
-            //    DbCommand comm = con2.CreateCommand();
-
-            //    comm.CommandText = $"INSERT INTO CashIntegration (IDEvent, ServerID, ServerMessage, ServerStatus) " +
-            //    $"VALUES ({id}, {response.TransactionId}, '{response.Message}', {response.StatusCode})";
-            //    comm.ExecuteScalar();
-            //    con2.Close();
-            //}
-            //model.Database.Connection.Close();
-            #endregion
-
-
-            List<CashWId> cashes = GetDataToSend();
-            if (cashes != null)
+            while (true)
             {
-
-
-                DbConnection con = model.Database.Connection;
-                con.Open();
-
-                DbCommand comm = con.CreateCommand();
-
-
-                foreach (CashWId c in cashes)
+                Console.Clear();
+                try
                 {
-                    Cash cash = new Cash();
-                    cash.Date = c.Date;
-                    cash.CashIncome = c.CashIncome;
-                    cash.Code = c.Code;
+                    cashes = GetDataToSend();
+                    if (cashes != null)
+                    {
+                        DbConnection con = model.Database.Connection;
+                        con.Open();
 
-                    string data = JsonConvert.SerializeObject(c);
-                    IntegrationResponse response = HttpSender.SendCash("https://api.myeco24.ru/transactions/post/cash", data, true);
-                    Console.WriteLine(response.StatusCode);
-                    //IntegrationResponse response = new IntegrationResponse();
-                    //response.StatusCode = 200;
-                    //response.TransactionId = 88;
+                        DbCommand comm = con.CreateCommand();
 
-                    comm.CommandText = $"INSERT INTO CashIntegration (IDEvent, ServerID, ServerMessage, ServerStatus) " +
-                    $"VALUES ({c.ID}, {response.TransactionId}, '{response.Message}', {response.StatusCode})";
-                    comm.ExecuteScalar();
+                        foreach (CashWId c in cashes)
+                        {
+                            Cash cash = new Cash();
+                            cash.Date = c.Date;
+                            cash.CashIncome = c.CashIncome;
+                            cash.Code = c.Code;
+
+                            string data = JsonConvert.SerializeObject(c);
+                            IntegrationResponse response = HttpSender.SendCash("https://api.myeco24.ru/transactions/post/cash", data, true);
+                            Console.WriteLine(response.StatusCode);
+                            //IntegrationResponse response = new IntegrationResponse();
+                            //response.StatusCode = 200;
+                            //response.TransactionId = 88;
+
+                            comm.CommandText = $"INSERT INTO CashIntegration (IDEvent, ServerID, ServerMessage, ServerStatus) " +
+                            $"VALUES ({c.ID}, {response.TransactionId}, '{response.Message}', {response.StatusCode})";
+                            comm.ExecuteScalar();
+                        }
+                        con.Close();
+                        cashes.Clear();
+                    }
                 }
-                con.Close();
+                catch (Exception e)
+                {
+                    Logger.Log.Error(e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+                }
+                Console.WriteLine(GetLastTime());
+                Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"));
+                Console.WriteLine("sleeping...z.z.z.");
+                System.Threading.Thread.Sleep(60000);
             }
 
         }
@@ -155,8 +95,7 @@ namespace CashIntegration
                     ") t" +
                     " left join CashIntegration ci on ci.IDEvent = t.IDEvent " +
                     "where 1=1" +
-                    " and ci.IDEvent is NULL " +
-                    "and t.DTime < CAST(GETDATE() AS date)";
+                    " and ci.IDEvent is NULL";
                 DbDataReader reader = command.ExecuteReader();
 
                 if (reader.HasRows)
@@ -199,6 +138,49 @@ namespace CashIntegration
                 model.Database.Connection.Close();
                 return null;
             }
+        }
+
+        public static string GetLastTime()
+        {
+            string result = "";
+
+            Logger.InitLogger();
+
+            try
+            {
+                ModelDb model = new ModelDb();
+
+                DbConnection con = model.Database.Connection;
+                con.Open();
+
+                DbCommand command = con.CreateCommand();
+
+                command.CommandText = "select " +
+                    "p.Code" +
+                    ", max(e.DTime) time " +
+                    "from CashIntegration ci " +
+                    "join Event e on e.IDEvent = ci.IDEvent " +
+                    "join Posts p on p.IDPost = e.IDPost " +
+                    "group by p.Code " +
+                    "order by p.Code";
+
+                DbDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        result += reader.GetString(reader.GetOrdinal("Code")) + ": " + reader.GetDateTime(reader.GetOrdinal("time")) + Environment.NewLine;
+                    }
+                }
+
+            }
+            catch(Exception e)
+            {
+                Logger.Log.Error(e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+            }
+
+            return result;
         }
     }
 }
