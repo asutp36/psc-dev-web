13053336d6fef1c9927d4cbfd6fc8be631cc3cd9
neckbor <neckbor@mail.ru>
2020-04-22 00:26:24 +0300

    - добавил конфигурацию получателей (132 - М13, отчёт);

--- service/ReportNotificationWhattsapp/ReportNotificationWhattsapp/Program.cs
+++ service/ReportNotificationWhattsapp/ReportNotificationWhattsapp/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using ReportNotificationWhattsapp.Models;
 using ReportNotificationWhattsapp.Supplies;
 using Newtonsoft.Json;
+using System.Configuration;
 
 namespace ReportNotificationWhattsapp
 {
@@ -15,44 +16,74 @@ namespace ReportNotificationWhattsapp
         static void Main(string[] args)
         {
             Logger.InitLogger();
-
-            ModelDb model = new ModelDb();
-
-            SqlParameter wash = new SqlParameter("@wash", System.Data.SqlDbType.NVarChar);
-            wash.Value = "М13";
-            SqlParameter date = new SqlParameter("@date", System.Data.SqlDbType.DateTime);
-            date.Value = DateTime.Now.AddDays(-1).Date;
-
-            List<GetDayReportIncrease_Result> result = model.Database.SqlQuery<GetDayReportIncrease_Result>("GetDayReportIncrease @wash, @date", wash, date).ToList();
-
-            List<string> phones = new List<string>();
-            //phones.Add("79050493285-1571938869@g.us");
-            phones.Add("79601199949@c.us");
-            phones.Add("79147066868@c.us");
-            phones.Add("79107424085@c.us");            
-            phones.Add("79202131085@c.us");
-
-            string body = result[0].msg.Replace(" П", "\nП").Replace(": ", ":\n").Insert(result[0].msg.IndexOf("за ") + 14, "\n");         
-
-            foreach (string chat in phones)
+            try
             {
-                MessageToSend message = new MessageToSend();
-                message.body = body;
-                message.chatId = chat;
-
-                string json = JsonConvert.SerializeObject(message);
-                ResponseSendMessage response = WhattsAppSender.SendMessage(json);
-
-                if (response.sent)
+                string toLog = "";
+                for (int i = 0; i < args.Length; i++)
                 {
-                    Console.WriteLine("ok");
-                    Logger.Log.Debug("Main: Сообщение отправлено: " + response.message + Environment.NewLine + json + Environment.NewLine);
+                    toLog += ' ' + args[i];
                 }
-                else
+
+
+                Logger.Log.Debug("Запуск с параметрами:" + toLog + ". Количество: " + args.Length);
+
+                // номер списка получателей
+                String recipientsGroup = args[0];
+                // код мойки
+                String washCode = "М" + (int.Parse(args[0]) / 10).ToString();
+
+                // Если определены все аргументы
+                if (!String.IsNullOrEmpty(recipientsGroup))
                 {
-                    Console.WriteLine("!ok");
-                    Logger.Log.Error("Main: Сообщение не отправлено: " + Environment.NewLine + response.message + Environment.NewLine + json + Environment.NewLine);
+                    // Определить получателей
+                    String smsRecipients = ConfigurationManager.AppSettings["waRecipients" + recipientsGroup];
+                    if (String.IsNullOrEmpty(smsRecipients))
+                    {
+                        // не найден список получателей
+                        Logger.Log.Error(String.Format("{0} Не определены получатели group = {1}", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"), recipientsGroup));
+                    }
+
+                    String[] recipients = smsRecipients.Split(new Char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    List<GetDayReportIncrease_Result> mtext = new List<GetDayReportIncrease_Result>();
+
+                    using (ModelDb model = new ModelDb())
+                    {
+
+                        SqlParameter wash = new SqlParameter("@wash", System.Data.SqlDbType.NVarChar);
+                        wash.Value = washCode;
+                        SqlParameter date = new SqlParameter("@date", System.Data.SqlDbType.DateTime);
+                        date.Value = DateTime.Now.AddDays(-1).Date;
+
+                        mtext = model.Database.SqlQuery<GetDayReportIncrease_Result>("GetDayReportIncrease @wash, @date", wash, date).ToList();
+                    }
+
+                    string body = mtext[0].msg.Replace(" П", "\nП").Replace(": ", ":\n").Insert(mtext[0].msg.IndexOf("за ") + 14, "\n");
+
+                    foreach (string chat in recipients)
+                    {
+                        MessageToSend message = new MessageToSend();
+                        message.body = body;
+                        message.chatId = chat;
+
+                        string json = JsonConvert.SerializeObject(message);
+                        ResponseSendMessage response = WhattsAppSender.SendMessage(json);
+
+                        if (response.sent)
+                        {
+                            Console.WriteLine("ok");
+                            Logger.Log.Debug("Main: Сообщение отправлено: " + response.message + Environment.NewLine + json + Environment.NewLine);
+                        }
+                        else
+                        {
+                            Console.WriteLine("!ok");
+                            Logger.Log.Error("Main: Сообщение не отправлено: " + Environment.NewLine + response.message + Environment.NewLine + json + Environment.NewLine);
+                        }
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error(e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
             }
         }
     }
