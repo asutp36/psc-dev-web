994ec806b34b73ee566e3a7b28d3439525e1bbb8
neckbor <neckbor@mail.ru>
2020-05-16 19:37:02 +0300

    - добавил ранее некомиченный файл скалярного ответа от сервера;

---
+++ webservice/PostControllingService/PostControllingService/Controllers/Supplies/GetScalarResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace PostControllingService.Controllers.Supplies
+{
+    public class GetScalarResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Result { get; set; }
+
+        public GetScalarResponse(HttpStatusCode code, string result)
+        {
+            this.StatusCode = code;
+            this.Result = result;
+        }
+    }
+}
