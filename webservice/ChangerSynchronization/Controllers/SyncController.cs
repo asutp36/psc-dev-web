1eccbd53b4fa88844276c31e294f7c14cc7300e1
neckbor <neckbor@mail.ru>
2020-07-22 14:48:20 +0300

    - changersync: заглушки функций;

--- webservice/ChangerSynchronization/Controllers/SyncController.cs
+++ webservice/ChangerSynchronization/Controllers/SyncController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChangerSynchronization.Controllers.Supplies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +12,44 @@ namespace ChangerSynchronization.Controllers
     [ApiController]
     public class SyncController : ControllerBase
     {
+        /// <summary>
+        /// Синхронизация события разменника
+        /// </summary>
+        /// <param name="model">Большое событие на разменнике</param>
+        /// <returns></returns>
+        /// <response code="400">Входные данные некорректны</response>
+        /// <response code="200">Ок, в теле айдишники</response>
         [HttpPost("event")]
-        public IActionResult PostEvent()
+        public IActionResult PostEvent(EventChanger model)
         {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+
+
             return Ok();
+        }
+
+        private void WriteEventChanger()
+        {
+
+        }
+
+        private void WriteEventChangerIncrease()
+        {
+
+        }
+
+        private void WriteEventChangerOut()
+        {
+
+        }
+
+        private void WriteEventChangerAcquiring()
+        {
+
         }
     }
 }
