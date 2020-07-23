413a9e35d0f25af143112bc440fc3b97ca857083
neckbor <neckbor@mail.ru>
2020-07-23 18:23:32 +0300

    - changersync: добавил обработку стандартных ошибок в PostEventChanger;

--- webservice/ChangerSynchronization_framework/Controllers/SyncController.cs
+++ webservice/ChangerSynchronization_framework/Controllers/SyncController.cs
@@ -1,4 +1,6 @@
 ﻿using ChangerSynchronization_framework.Controllers.Supplies;
+using ChangerSynchronization_framework.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +12,36 @@ namespace ChangerSynchronization_framework.Controllers
 {
     public class SyncController : ApiController
     {
+        private ModelDb _model = new ModelDb();
         public HttpResponseMessage PostEventChanger([FromBody]EventChangerFull model)
         {
-            if (!ModelState.IsValid)
-            {
-                return Request.CreateResponse(HttpStatusCode.NoContent);
-            }
+            Logger.InitLogger();
 
-            return Request.CreateResponse(HttpStatusCode.OK);
+            try
+            {
+                Logger.Log.Debug("PostEventChanger: запуск с параметрами" + JsonConvert.SerializeObject(model));
+
+                if (!ModelState.IsValid)
+                {
+                    Logger.Log.Error("PostEventChanger: модель не прошла валидацию." + Environment.NewLine);
+                    return Request.CreateResponse(HttpStatusCode.NoContent);
+                }
+
+                if (!_model.Database.Exists())
+                {
+                    Logger.Log.Error("PostEventChanger: база данных не найдена" + Environment.NewLine);
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError);
+                }
+
+
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error("PostEventChanger: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
