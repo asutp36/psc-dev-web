37f92185696393746e22520fa39b685bf0759aa8
neckbor <neckbor@mail.ru>
2020-07-24 13:04:20 +0300

    - changersync: написал добавление в бд EventChanger;

---
+++ webservice/ChangerSynchronization_framework/Controllers/Supplies/DbInsertResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChangerSynchronization_framework.Controllers.Supplies
+{
+    public class DbInsertResult
+    {
+        public int? serverId { get; set; }
+        public string serverMassege { get; set; }
+    }
+}
--- webservice/ChangerSynchronization_framework/Controllers/SyncController.cs
+++ webservice/ChangerSynchronization_framework/Controllers/SyncController.cs
@@ -3,6 +3,7 @@ using ChangerSynchronization_framework.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -13,6 +14,16 @@ namespace ChangerSynchronization_framework.Controllers
     public class SyncController : ApiController
     {
         private ModelDb _model = new ModelDb();
+
+        /// <summary>
+        /// Синхронизация события на разменнике
+        /// </summary>
+        /// <param name="model">Полная информация о событии</param>
+        /// <returns></returns>
+        /// <response code="201">Событие записано</response>
+        /// <response code="503">Не получилось добавить EventChanger</response>
+        [HttpPost]
+        [ActionName("event")]
         public HttpResponseMessage PostEventChanger([FromBody]EventChangerFull model)
         {
             Logger.InitLogger();
@@ -33,14 +44,55 @@ namespace ChangerSynchronization_framework.Controllers
                     return Request.CreateResponse(HttpStatusCode.InternalServerError);
                 }
 
+                DbInsertResult eventChangerResult = WriteEventChanger(model);
 
+                if (eventChangerResult.serverId != null && eventChangerResult.serverId > 0)
+                {
 
-                return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    Logger.Log.Error("PostEventChanger: не удалось записать в базу EventChanger." + Environment.NewLine);
+                    return Request.CreateResponse((HttpStatusCode)503, "Не удалось записать в базу EventChanger");
+                }
+
+                return Request.CreateResponse(HttpStatusCode.Created);
             }
             catch (Exception e)
             {
                 Logger.Log.Error("PostEventChanger: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
                 return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private DbInsertResult WriteEventChanger(EventChangerFull eventFull)
+        {
+            Logger.InitLogger();
+            try
+            {
+                _model.Database.Connection.Open();
+                Logger.Log.Debug("WriteEventChanger: connection state: " + _model.Database.Connection.State.ToString());
+
+                DbCommand command = _model.Database.Connection.CreateCommand();
+
+                command.CommandText = $"INSERT INTO EventChanger (IDChanger, IDEventChangerKind, DTime) " +
+                    $"VALUES ((select IDChanger from Changers c where c.Code == '{eventFull.changer}'), " +
+                    $"(select IDEventChangerKind from EventChangerKind evk where evk.Code == '{eventFull.eventKindCode}')," +
+                    $"'{eventFull.dtime}'); " +
+                    $"SELECT SCOPE_IDENTITY();";
+
+                Logger.Log.Debug("WriteEventChanger: command is:\n" + command.CommandText);
+
+                var id = command.ExecuteScalar();
+
+                Logger.Log.Debug("WriteEventChanger: EventChanger добавлен. id = " + id.ToString());
+
+                return new DbInsertResult { serverId = int.Parse(id.ToString()) };
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error("WriteEventChanger: ошибка при записи в базу.\n" + e.Message + Environment.NewLine + e.StackTrace);
+                return new DbInsertResult { serverMassege = "Приём ок. Ошибка записи в базу" };
             }
         }
     }
