5789cdc812dd57848fc2ba5847cefc65b46f2236
neckbor <neckbor@mail.ru>
2020-08-05 13:09:11 +0300

    - добавил контроллер и модель отправки карты, написал заглушку функции;

---
+++ webservice/ChangerSynchronization_framework/Controllers/CardsController.cs
@@ -0,0 +1,45 @@
+﻿using ChangerSynchronization_framework.Controllers.Supplies;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChangerSynchronization_framework.Controllers
+{
+    [Route("api/cards")]
+    public class CardsController : ApiController
+    {
+        /// <summary>
+        /// Запись события о выпущенной карте
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <response code="201">Запись создана</response>
+        /// <response code="500">Внутренняя ошибка</response>
+        /// <response code="400">Модель не прошла влидацию</response>
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]NewCard model)
+        {
+            Logger.InitLogger();
+
+            if (!ModelState.IsValid)
+            {
+                Logger.Log.Error("CardsPost: модель не прошла валидацию\n" + JsonConvert.SerializeObject(model) + Environment.NewLine);
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try 
+            {
+                return Request.CreateResponse(HttpStatusCode.Created);
+            }
+            catch (Exception e)
+            {
+                Logger.Log.Error("CardsPost: произошла внутренняя ошибка\n" + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
---
+++ webservice/ChangerSynchronization_framework/Controllers/Supplies/NewCard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChangerSynchronization_framework.Controllers.Supplies
+{
+    /// <summary>
+    /// Модель записи выпущенной карты разменника
+    /// </summary>
+    public class NewCard
+    {
+        /// <summary>
+        /// Код разменника
+        /// </summary>
+        [Required]
+        public string changer { get; set; }
+
+        /// <summary>
+        /// Номер выпущенной карты
+        /// </summary>
+        [Required]
+        public string cardNum { get; set; }
+
+
+        /// <summary>
+        /// Телефон владельца
+        /// </summary>
+        [Required]
+        public string phone { get; set; }
+
+        /// <summary>
+        /// ID в локальной базе
+        /// </summary>
+        [Required]
+        public int localizedID { get; set; }
+    }
+}
