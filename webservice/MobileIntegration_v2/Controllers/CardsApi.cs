4db866cf5eb9c19d363d0cf4aa4ead938301a93e
neckbor <neckbor@mail.ru>
2020-08-06 11:58:47 +0300

    - накидал контроллер Mobile и классы CardsApi.cs, PostApi.cs;

---
+++ webservice/MobileIntegration_v2/Controllers/CardsApi.cs
@@ -0,0 +1,23 @@
+﻿using MobileIntegration_v2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileIntegration_v2.Controllers
+{
+    public class CardsApi
+    {
+        private ModelDbContext _model = new ModelDbContext();
+
+        public void WriteIncrease() { }
+
+        public void GetBalance() { }
+
+        public void UpdatePhone() { }
+
+        public void WriteNewCard() { }
+
+        public void GetCardsByPhone() { }
+    }
+}
---
+++ webservice/MobileIntegration_v2/Controllers/PostApi.cs
@@ -0,0 +1,17 @@
+﻿using MobileIntegration_v2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileIntegration_v2.Controllers
+{
+    public class PostApi
+    {
+        private ModelDbContext _model = new ModelDbContext();
+
+        public void Start() { }
+
+        public void Stop() { }
+    }
+}
