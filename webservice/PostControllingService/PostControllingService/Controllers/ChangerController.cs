62910a1e86101975ee86b33fd8dbb54a611b8b41
neckbor <neckbor@mail.ru>
2020-08-14 11:25:44 +0300

    - написал функцию запроса счётчиков разменника;

---
+++ webservice/PostControllingService/PostControllingService/Controllers/ChangerController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+using PostControllingService.Controllers.Supplies;
+using PostControllingService.Models;
+
+namespace PostControllingService.Controllers
+{
+    public class ChangerController : ApiController
+    {
+        ModelDb _model = new ModelDb();
+
+        /// <summary>
+        /// Получить счётчики на разменнике
+        /// </summary>
+        /// <param name="changer">Код разменника</param>
+        /// <returns></returns>
+        /// <response code="200">Ок</response>
+        /// <response code="400">Некорректное значение</response>
+        /// <response code="424">Ответ разменника неудовлетворительный или нет связи</response>
+        /// <response code="404">Разменник не найден</response>
+        [HttpGet]
+        [ActionName("counters")]
+        public HttpResponseMessage GetCounters(string changer)
+        {
+            Logger.InitLogger();
+
+            Logger.Log.Debug("GetCounters: запуск с параметром\n" + changer);
+
+            if (changer.Length == 0)
+            {
+                Logger.Log.Error("GetCounters: входное значение некорректное" + Environment.NewLine);
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            string ip = _model.Device.Where(d => d.Code.Equals(changer)).FirstOrDefault().IpAddress;
+
+            if (ip == null || ip.Length == 0)
+            {
+                Logger.Log.Error("GetCounters: разменник не найден" + Environment.NewLine);
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            HttpSenderResponse response = HttpSender.SendGet("http://" + ip + "/counters");
+
+            Logger.Log.Debug("GetCounters: ответ от разменника:\n" + JsonConvert.SerializeObject(response));
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                ChangerCounters counters = JsonConvert.DeserializeObject<ChangerCounters>(response.Message);
+                return Request.CreateResponse(HttpStatusCode.OK, counters);
+            }
+
+            return Request.CreateResponse((HttpStatusCode)424);
+        }
+    }
+}
---
+++ webservice/PostControllingService/PostControllingService/Controllers/Supplies/ChangerCounters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostControllingService.Controllers.Supplies
+{
+    public class ChangerCounters
+    {
+        public int m10 { get; set; }
+        public int b50 { get; set; }
+        public int b100 { get; set; }
+        public int b200 { get; set; }
+        public int b500 { get; set; }
+        public int b1000 { get; set; }
+        public int b2000 { get; set; }
+        public int box1_50 { get; set; }
+        public int box2_100 { get; set; }
+        public int box3_50 { get; set; }
+        public int box4_100 { get; set; }
+        public int badCards { get; set; }
+        public int availableCards { get; set; }
+        public string bill { get; set; }
+        public string coiner { get; set; }
+        public string bank { get; set; }
+        public string oddMoney { get; set; }
+        public string hopper { get; set; }
+        public string cards { get; set; }
+        public string issueCards { get; set; }
+        public string fr { get; set; }
+        public string printCheck { get; set; }
+    }
+}
