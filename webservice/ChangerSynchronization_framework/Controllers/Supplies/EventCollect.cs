10ac3fe68f6d9f1667d36e8f28839083478ed920
neckbor <neckbor@mail.ru>
2020-08-17 18:27:07 +0300

    - добавил класс события инкассации;

---
+++ webservice/ChangerSynchronization_framework/Controllers/Supplies/EventCollect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChangerSynchronization_framework.Controllers.Supplies
+{
+    public class EventCollect : Event
+    {
+        public int m10 { get; set; }
+        public int b50 { get; set; }
+        public int b100 { get; set; }
+        public int b200 { get; set; }
+        public int b500 { get; set; }
+        public int b1000 { get; set; }
+        public int b2000 { get; set; }
+        public int box1_50 { get; set; }
+        public int box2_100 { get; set; }
+        public int box3_50 { get; set; }
+        public int box4_100 { get; set; }
+        public int badCards { get; set; }
+        public int availibleCards { get; set; }
+    }
+
+}
