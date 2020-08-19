c260f5a64c5861f988cf3102de4eaf8ca4748239
neckbor <neckbor@mail.ru>
2020-08-19 14:50:59 +0300

    - комичу модели новой карты;

---
+++ webservice/CardsMobileService/Controllers/Supplies/NewCardFromChanger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardsMobileService.Controllers.Supplies
+{
+    public class NewCardFromChanger : NewCardModel
+    {
+        [Required]
+        public string changer { get; set; }
+        [Required]
+        public string localizedID { get; set; }
+        [Required]
+        public int amount { get; set; }
+    }
+}
