93970a9e5eec3de5e3bb4b4bb6b768a0483c336a
neckbor <neckbor@mail.ru>
2020-08-21 13:02:24 +0300

    - переименовал эндпоинты в соответствии с вариантом на фреймворке; - добавил модель для запроса баланса с мобилки;

--- webservice/CardsMobileService/Controllers/MobileController.cs
+++ webservice/CardsMobileService/Controllers/MobileController.cs
@@ -11,7 +11,7 @@ using Newtonsoft.Json;
 
 namespace CardsMobileService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/mobile")]
     [ApiController]
     public class MobileController : ControllerBase
     {
@@ -38,23 +38,23 @@ namespace CardsMobileService.Controllers
         [HttpPost("increase")]
         public IActionResult PostIncrease(IncreaseFromMobile model)
         {
-            _logger.LogDebug("PostIncrease: запуск с параметрами\n" + JsonConvert.SerializeObject(model));
+            _logger.LogDebug("MOBILE PostIncrease: запуск с параметрами\n" + JsonConvert.SerializeObject(model));
 
             if (!ModelState.IsValid)
             {
-                _logger.LogError("PostIncrease: модель не прошла валидацию" + Environment.NewLine);
+                _logger.LogError("MOBILE PostIncrease: модель не прошла валидацию" + Environment.NewLine);
                 return BadRequest();
             }
 
             if (!CryptHash.CheckHashCode(model.hash, model.dtime))
             {
-                _logger.LogError("PostIncrease: хэш не прошёл проверку" + Environment.NewLine);
+                _logger.LogError("MOBILE PostIncrease: хэш не прошёл проверку" + Environment.NewLine);
                 return Unauthorized();
             }
 
             if (!_cardsApi.IsExist(model.cardNum))
             {
-                _logger.LogError("PostIncrease: карта не найдена" + Environment.NewLine);
+                _logger.LogError("MOBILE PostIncrease: карта не найдена" + Environment.NewLine);
                 return NotFound();
             }
 
@@ -72,60 +72,34 @@ namespace CardsMobileService.Controllers
                 
                 if (serverID == -1)
                 {
-                    _logger.LogError("PostIncrease: транзакция не прошла" + Environment.NewLine);
+                    _logger.LogError("MOBILE PostIncrease: транзакция не прошла" + Environment.NewLine);
                     return StatusCode(503, "Ошибка записи в базу");
                 }
 
-                _logger.LogDebug("PostIncrease: пополнение записано id = " + serverID + Environment.NewLine);
+                _logger.LogDebug("MOBILE PostIncrease: пополнение записано id = " + serverID + Environment.NewLine);
 
-                //return Created("/mobile/increase", serverID);
                 return StatusCode(201);
             }
             catch (Exception e)
             {
-                _logger.LogCritical("PostIncrease: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+                _logger.LogCritical("MOBILE PostIncrease: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
                 return StatusCode(500);
             }
         }
 
-        [HttpGet("balance")]
-        public IActionResult GetBalance(string cardNum)
+        [HttpPost("get_balance")]
+        public IActionResult GetBalance(GetBalanceFromMobile model)
         {
-            if (!CryptHash.CheckHashCode("hash", "dtime"))
+            _logger.LogInformation("MOBILE GetBalance: запуск с параметрами:\n" + JsonConvert.SerializeObject(model));
+            if (!CryptHash.CheckHashCode(model.hash, model.dtime))
             {
+                _logger.LogError("MOBILE GetBalance: хэш не прошёл проверку" + Environment.NewLine);
                 return Unauthorized();
             }
 
-            if (cardNum == null || cardNum.Length < 5)
+            if (!ModelState.IsValid)
             {
                 return BadRequest();
-            }
-
-            return Ok();
-        }
-
-        [HttpGet("cards")]
-        public IActionResult GetCards(string phone)
-        {
-            if (!CryptHash.CheckHashCode("hash", "dtime"))
-            {
-                return Unauthorized();
-            }
-
-            if (phone == null || phone.Length < 11)
-            {
-                return BadRequest();
-            }
-
-            return Ok();
-        }
-
-        [HttpPut("phone")]
-        public IActionResult UpdatePhone()
-        {
-            if (!CryptHash.CheckHashCode("hash", "dtime"))
-            {
-                return Unauthorized();
             }
 
             return Ok();
@@ -145,32 +119,32 @@ namespace CardsMobileService.Controllers
         /// <response code="422">Сумма недостаточна для включения</response>
         /// <response code="423">Пост занят</response>
         /// <response code="424">Нет связи с постом</response>
-        [HttpPost("start")]
+        [HttpPost("start_post")]
         public IActionResult StartPost(StartModelMobile model)
         {
-            _logger.LogInformation("StartPost: запуск с параметрами\n" + JsonConvert.SerializeObject(model));
+            _logger.LogInformation("MOBILE StartPost: запуск с параметрами\n" + JsonConvert.SerializeObject(model));
 
             if (!CryptHash.CheckHashCode(model.hash, model.dtime))
             {
-                _logger.LogError("StartPost: хэш не прошёл проверку" + Environment.NewLine);
+                _logger.LogError("MOBILE StartPost: хэш не прошёл проверку" + Environment.NewLine);
                 return Unauthorized();
             }
 
             if (!ModelState.IsValid)
             {
-                _logger.LogError("StartPost: модель не прошла валидацию" + Environment.NewLine);
+                _logger.LogError("MOBILE StartPost: модель не прошла валидацию" + Environment.NewLine);
                 return BadRequest();
             }
 
             if (!_cardsApi.IsExist(model.cardNum))
             {
-                _logger.LogError("StartPost: карты с номером {0} не существует" + Environment.NewLine);
+                _logger.LogError("MOBILE StartPost: карты с номером {0} не существует" + Environment.NewLine);
                 return NotFound();
             }
 
             if (!_postApi.IsExist(model.post))
             {
-                _logger.LogError("StartPost: не найден код поста" + Environment.NewLine);
+                _logger.LogError("MOBILE StartPost: не найден код поста" + Environment.NewLine);
                 return StatusCode(405);
             }
 
@@ -179,22 +153,22 @@ namespace CardsMobileService.Controllers
             switch (result)
             {
                 case "weak":
-                    _logger.LogError("StartPost: сумма недостаточна для включения" + Environment.NewLine);
+                    _logger.LogError("MOBILE StartPost: сумма недостаточна для включения" + Environment.NewLine);
                     return StatusCode(422);
 
                 case "unavailible":
-                    _logger.LogError("StartPost: не удалось установить связь с постом" + Environment.NewLine);
+                    _logger.LogError("MOBILE StartPost: не удалось установить связь с постом" + Environment.NewLine);
                     return StatusCode(424);
 
                 case "busy":
-                    _logger.LogError("StartPost: пост занят" + Environment.NewLine);
+                    _logger.LogError("MOBILE StartPost: пост занят" + Environment.NewLine);
                     return StatusCode(423);
                 case "ok":
-                    _logger.LogError("StartPost: мойка начата" + Environment.NewLine);
+                    _logger.LogError("MOBILE StartPost: мойка начата" + Environment.NewLine);
                     return Ok();
 
                 default:
-                    _logger.LogError("StartPost: " + result + Environment.NewLine);
+                    _logger.LogError("MOBILE StartPost: " + result + Environment.NewLine);
                     return StatusCode(417);
             }
         }
@@ -210,26 +184,26 @@ namespace CardsMobileService.Controllers
         /// <response code="409">На этот номер уже записана карта</response>
         /// <response code="417">Ошибка при записи в базу</response>
         /// <response code="500">Внутренняя ошибка</response>
-        [HttpPost("card")]
+        [HttpPost("new_card")]
         public IActionResult PostCard(NewCardFromMobile model)
         {
-            _logger.LogInformation("PostCard: запуск с параметрами: " + JsonConvert.SerializeObject(model));
+            _logger.LogInformation("MOBILE PostCard: запуск с параметрами: " + JsonConvert.SerializeObject(model));
 
             if (!CryptHash.CheckHashCode(model.hash, model.dtime))
             {
-                _logger.LogError("PostCard: хэш не прошёл проверку" + Environment.NewLine);
+                _logger.LogError("MOBILE PostCard: хэш не прошёл проверку" + Environment.NewLine);
                 return Unauthorized();
             }
 
             if (!ModelState.IsValid)
             {
-                _logger.LogError("PostCard: модель не прошла валидацию" + Environment.NewLine);
+                _logger.LogError("MOBILE PostCard: модель не прошла валидацию" + Environment.NewLine);
                 return BadRequest();
             }
 
             if (_cardsApi.GetCardsByPhone(model.phone).Count > 0)
             {
-                _logger.LogError("PostCard: на этот номер уже записаны карты" + Environment.NewLine);
+                _logger.LogError("MOBILE PostCard: на этот номер уже записаны карты" + Environment.NewLine);
                 return Conflict();
             }
 
@@ -247,13 +221,13 @@ namespace CardsMobileService.Controllers
 
                 if (serverID > 0)
                 {
-                    _logger.LogDebug("PostCard: удачно всё запсано. SeverID = " + serverID + Environment.NewLine);
-                    return Created("/api/mobile/card", null);
+                    _logger.LogDebug("MOBILE PostCard: удачно всё запсано. SeverID = " + serverID + Environment.NewLine);
+                    return StatusCode(201);
                 }
             }
             catch(Exception e)
             {
-                _logger.LogError("PostCard: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+                _logger.LogError("MOBILE PostCard: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
                 return StatusCode(417, "Не найдена база данных");
             }
 
