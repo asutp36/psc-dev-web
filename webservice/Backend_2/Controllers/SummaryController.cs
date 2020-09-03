3e53cb8ca031297caaaaa66d1773de69fa82fdda
neckbor <neckbor@mail.ru>
2020-09-03 13:56:19 +0300

    - закомментил всё про логгирование, всё работает на сервере;

--- webservice/Backend_2/Controllers/SummaryController.cs
+++ webservice/Backend_2/Controllers/SummaryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Backend_2.Controllers
 {
@@ -11,6 +12,13 @@ namespace Backend_2.Controllers
     [ApiController]
     public class SummaryController : ControllerBase
     {
+        //ILogger<SummaryController> _logger;
+
+        //public SummaryController(ILogger<SummaryController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
         [HttpGet]
         public IActionResult Get()
         {
---
+++ webservice/Backend_2/Controllers/Supplies/Logger/FileLoggerExtentions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_2.Controllers.Supplies.Logger
+{
+    public static class FileLoggerExtentions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory,
+                                        string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
