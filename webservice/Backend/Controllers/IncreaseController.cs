5cbbf4b6d6cbe61a83891e3a1a5ca9c02b704923
neckbor <neckbor@mail.ru>
2020-09-03 11:31:09 +0300

    - закомментил метод и логгер;

--- webservice/Backend/Controllers/IncreaseController.cs
+++ webservice/Backend/Controllers/IncreaseController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Backend.Controllers
 {
@@ -15,6 +16,12 @@ namespace Backend.Controllers
     public class IncreaseController : ControllerBase
     {
         ModelDbContext _model = new ModelDbContext();
+        //ILogger<IncreaseController> _logger;
+
+        //public IncreaseController(ILogger<IncreaseController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         [HttpGet("durperiod")]
         public IActionResult GetDurPeriod(string startDate, string endDate, string login, string washCode)
@@ -36,24 +43,25 @@ namespace Backend.Controllers
             return Ok(ssum.Value);
         }
 
-        [HttpGet("bywashs")]
-        public IActionResult GetByWashs(string startDate, string endDate, int regionCode = 0, string washCode = "")
-        {
-            try
-            {
-                SqlParameter p_dateBeg = new SqlParameter("@p_DateBeg", startDate);
-                SqlParameter p_DateEnd = new SqlParameter("@p_DateEnd", endDate);
-                SqlParameter p_RegionCode = new SqlParameter("@p_RegionCode", regionCode);
-                SqlParameter p_WashCode = new SqlParameter("@p_WashCode", washCode);
+        //[HttpGet("bywashs")]
+        //public IActionResult GetByWashs(string startDate, string endDate, int regionCode = 0, string washCode = "")
+        //{
+        //    try
+        //    {
+        //        SqlParameter p_dateBeg = new SqlParameter("@p_DateBeg", startDate);
+        //        SqlParameter p_DateEnd = new SqlParameter("@p_DateEnd", endDate);
+        //        SqlParameter p_RegionCode = new SqlParameter("@p_RegionCode", regionCode);
+        //        SqlParameter p_WashCode = new SqlParameter("@p_WashCode", washCode);
 
-                var result = _model.Set<GetIncreaseByWashs_Result>().FromSqlRaw("GetIncreaseByWashs @p_DateBeg, @p_DateEnd, @p_RegionCode, @p_WashCode", p_dateBeg, p_DateEnd, p_RegionCode, p_WashCode);
+        //        var result = _model.Set<GetIncreaseByWashs_Result>().FromSqlRaw("GetIncreaseByWashs @p_DateBeg, @p_DateEnd, @p_RegionCode, @p_WashCode", p_dateBeg, p_DateEnd, p_RegionCode, p_WashCode);
 
-                return Ok(result);
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, e);
-            }
-        }
+        //        return Ok(result);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        _logger.LogError("GetByWashs: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+        //        return StatusCode(500);
+        //    }
+        //}
     }
 }
