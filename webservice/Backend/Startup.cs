93b7dae305f2eea09200407c9cb0b0cc35dbfcb3
neckbor <neckbor@mail.ru>
2020-09-03 17:52:55 +0300

    - Supplies раскидал ещё в папки по темам; - добавил всё для авторизации;

--- webservice/Backend/Startup.cs
+++ webservice/Backend/Startup.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Backend.Controllers.Supplies;
+using Backend.Controllers.Supplies.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Backend
@@ -29,6 +32,31 @@ namespace Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            // укзывает, будет ли валидироватьс€ издатель при валидации токена
+                            ValidateIssuer = true,
+                            // строка, представл€юща€ издател€
+                            ValidIssuer = AuthOptions.ISSUER,
+
+                            // будет ли валидироватьс€ потребитель токена
+                            ValidateAudience = true,
+                            // установка потребител€ токена
+                            ValidAudience = AuthOptions.AUDIENCE,
+                            // будет ли валидироватьс€ врем€ существовани€
+                            ValidateLifetime = true,
+
+                            // установка ключа безопасности
+                            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                            // валидаци€ ключа безопасности
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
+
             services.AddControllers();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
@@ -40,6 +68,20 @@ namespace Backend
                     Title = "Backend",
                     Description = "Ѕэкенд сайта",
                 });
+
+
+                c.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "«начение заголовка JWT авторизации.\n‘ормат: Bearer *token*",
+                });
+
+                c.OperationFilter<AuthOperationFilter>();
+
 
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
