7aa12d07cf3acbfbdf80ba94b15c3fc69025cfd9
neckbor <neckbor@mail.ru>
2020-09-11 12:10:40 +0300

    - mobile-int: добавил класс для выдачи технических карт;

---
+++ webservice/MobileIntegration/MobileIntegration/Controllers/Supplies/TechCards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileIntegration.Controllers.Supplies
+{
+    public class TechCards
+    {
+        public List<string> cleanUp { get; set; }
+        public List<string> collect { get; set; }
+        public List<string> doors { get; set; }
+        public List<string> service { get; set; }
+    }
+}
