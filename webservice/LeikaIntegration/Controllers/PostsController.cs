793e171932ec2725fb368fa82165c8af62deff22
neckbor <neckbor@mail.ru>
2020-10-20 18:14:58 +0300

    - leika: добавил функцию нахождения ip поста;

--- webservice/LeikaIntegration/Controllers/PostsController.cs
+++ webservice/LeikaIntegration/Controllers/PostsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LeikaIntegration.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,16 +12,45 @@ namespace LeikaIntegration.Controllers
     [ApiController]
     public class PostsController : ControllerBase
     {
+        private readonly ModelDbContext _model = new ModelDbContext();
+
         [HttpGet]
-        public IActionResult State()
+        public IActionResult State(string code)
         {
-            return Ok();
+            try 
+            {
+                string postIp = GetPosIp(code);
+                if (postIp.Equals(""))
+                {
+                    return NotFound();
+                }
+
+
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost]
         public IActionResult Start()
         {
             return Ok();
+        }
+
+        private string GetPosIp(string code)
+        {
+            try
+            {
+                return _model.Device.Find(_model.Posts.Where(p => p.Qrcode.Equals(code)).FirstOrDefault().Iddevice).IpAddress;
+            }
+            catch (NullReferenceException)
+            {
+                return "";
+            }
         }
     }
 }
