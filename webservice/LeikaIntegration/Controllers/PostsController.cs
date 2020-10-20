d7dcf350d8c0d410713fec06f2780778de29bb42
neckbor <neckbor@mail.ru>
2020-10-20 17:57:12 +0300

    - leika: добавил контроллер Posts с наброском функций;

---
+++ webservice/LeikaIntegration/Controllers/PostsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeikaIntegration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult State()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Start()
+        {
+            return Ok();
+        }
+    }
+}
