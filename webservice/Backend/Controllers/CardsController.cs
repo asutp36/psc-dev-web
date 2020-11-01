b996a16a334fb71121f1f88b85fa1f62724a3d21
neckbor <neckbor@mail.ru>
2020-11-01 12:35:31 +0300

    - backend: добавил классы и контроллер для старницы "карты";

---
+++ webservice/Backend/Controllers/CardsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Controllers.Supplies;
+using Backend.Controllers.Supplies.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("filters")]
+        public IActionResult GetFilters()
+        {
+            try
+            {
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, new Error(e.Message, "unexpected"));
+            }
+        }
+        
+    }
+}
---
+++ webservice/Backend/Controllers/Supplies/Filters/CardsFilters.cs
@@ -0,0 +1,50 @@
+﻿using Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers.Supplies.Filters
+{
+    public class CardsFilters
+    {
+        public List<CardTypeViewModel> cardTypes { get; set; }
+        public List<CardStatusViewModel> cardStatuses { get; set; }
+        public List<ActivationDeviceViewModel> activationDevices { get; set; }
+        public List<LastOperationDeviceViewModel> lastOperationDevices { get; set; }
+
+        private ModelDbContext _model = new ModelDbContext();
+
+        public CardsFilters()
+        {
+            cardTypes = GetCardTypes();
+            cardStatuses = GetCardStatuses();
+        }
+
+        private List<CardTypeViewModel> GetCardTypes()
+        {
+            List<CardTypeViewModel> result = new List<CardTypeViewModel>();
+
+            List<CardTypes> types = _model.CardTypes.ToList();
+            foreach(CardTypes ct in types)
+            {
+                result.Add(new CardTypeViewModel { code = ct.Code, name = ct.Name });
+            }
+
+            return result;
+        }
+
+        private List<CardStatusViewModel> GetCardStatuses()
+        {
+            List<CardStatusViewModel> result = new List<CardStatusViewModel>();
+
+            List<CardStatuses> statuses = _model.CardStatuses.ToList();
+            foreach(CardStatuses cs in statuses)
+            {
+                result.Add(new CardStatusViewModel { code = cs.Code, name = cs.Name });
+            }
+
+            return result;
+        }
+    }
+}
