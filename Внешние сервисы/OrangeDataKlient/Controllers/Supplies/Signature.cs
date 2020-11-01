c48e1b5ba1a1defedc78af311a4d36767096e119
neckbor <neckbor@mail.ru>
2020-11-01 12:36:31 +0300

    - orangedata: добавил проект и скачал штуки;

---
+++ Внешние сервисы/OrangeDataKlient/Controllers/Supplies/Signature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeDataKlient.Controllers.Supplies
+{
+    public class Signature
+    {
+        public string ComputeSignature(string document)
+        {
+            var data = Encoding.UTF8.GetBytes(document);
+
+            using (var rsa = RSA.Create())
+            {
+                rsa.FromXmlString(File.ReadAllText("rsa_2048_private_key.xml"));
+                return Convert.ToBase64String(rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1));
+            }
+        }
+
+    }
+}
