78c72ce1d2a2d203fc1dc98a41e7bebd24d8d01c
neckbor <neckbor@mail.ru>
2020-12-13 16:57:45 +0300

    - testconn: сделал функцию опроса постов мойки М13;

--- webservice/TestConnectionService/Controllers/WashesController.cs
+++ webservice/TestConnectionService/Controllers/WashesController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestConnectionService.Controllers.Supplies;
+using TestConnectionService.Models;
+using HttpResponse = TestConnectionService.Controllers.Supplies.HttpResponse;
 
 namespace TestConnectionService.Controllers
 {
@@ -16,12 +19,38 @@ namespace TestConnectionService.Controllers
         {
             try
             {
-                return Ok();
+                List<string> result = new List<string>();
+                List<string> ips = GetIPs("М 202");
+                foreach(string ip in ips)
+                {
+                    if (ip == null)
+                        continue;
+                    HttpResponse response = HttpSender.SendGet("http://" + ip + "/api/post/heartbeat");
+                    result.Add(response.StatusCode.ToString());
+                }
+
+                return Ok(result);
             }
             catch(Exception e)
             {
-                return StatusCode(500);
+                return StatusCode(500, e);
             }
+        }
+
+        private List<string> GetIPs(string washCode)
+        {
+            ModelDbContext model = new ModelDbContext();
+            List<string> result = new List<string>();
+
+            List<int?> devices = model.Posts.Where(p => p.Idwash == model.Washes.Where(w => w.Code == washCode).FirstOrDefault().Idwash).Select(d => d.Iddevice).ToList();
+            foreach (int? d in devices)
+            {
+                if(d != null)
+                {
+                    result.Add(model.Devices.Find(d).IpAddress);
+                }
+            }
+            return result;
         }
     }
 }
