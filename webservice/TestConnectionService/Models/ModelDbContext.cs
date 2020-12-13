c939074e2a85db77b5ef38b6f61973aca8fdbe62
neckbor <neckbor@mail.ru>
2020-12-13 15:41:23 +0300

    - testconn: добавил строку подключения к бд на сервере;

--- webservice/TestConnectionService/Models/ModelDbContext.cs
+++ webservice/TestConnectionService/Models/ModelDbContext.cs
@@ -30,6 +30,7 @@ namespace TestConnectionService.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=WashCompany;Trusted_Connection=True;");
+                //optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Initial Catalog=WashCompany;User Id=sa; Password=ora4paSS");
             }
         }
 
