17dd12856aa006b3a7f54507f823ad780d253b67
neckbor <neckbor@mail.ru>
2020-12-13 15:59:20 +0300

    - testconn: удалил стандартные файлы WeatherForecast; - добавил классы отправки хттп запросов и контроллер;

---
+++ webservice/TestConnectionService/Controllers/WashesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestConnectionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WashesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetConnection()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
