d8738baf89d76df727a082a4a5c2f000ea0238f2
neckbor <neckbor@mail.ru>
2020-12-17 19:45:06 +0300

    - testconn: коммит файла CacheService.cs;

---
+++ webservice/TestConnectionService/Services/CacheService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestConnectionService.Controllers.Supplies;
+
+namespace TestConnectionService.Services
+{
+    public class CacheService
+    {
+        private IMemoryCache _cache;
+
+        public CacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public string GetLastConnection(string post)
+        {
+            string lastConnection = null;
+            if (!_cache.TryGetValue(post, out lastConnection))
+            {
+                HttpResponse response = HttpSender.SendGet("https://yandex.com/time/sync.json?geo=213");
+
+                _cache.Set(post, response.ResultMessage);
+                lastConnection = response.ResultMessage;
+            }
+
+            return lastConnection;
+        }
+    }
+}
