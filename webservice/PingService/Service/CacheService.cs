f1e2e72152a65af484f3e96df54d0c4d9a18a70a
neckbor <neckbor@mail.ru>
2020-12-21 20:32:00 +0300

    - ping: добавил классы отправки запросов хттп, написал асинхронный метод обновления кэша;

--- webservice/PingService/Service/CacheService.cs
+++ webservice/PingService/Service/CacheService.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Memory;
+using PingService.Supplies;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PingService.Service
 {
     class CacheService
     {
+        private IMemoryCache _cache;
+        private object locker;
+
+        public CacheService (IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task UpdateCacheAsync()
+        {
+            while (true)
+            {
+                lock (locker)
+                {
+                    HttpResponse response = HttpSender.SendGet("https://yandex.com/time/sync.json?geo=213");
+
+                    _cache.Set("13-1", response.ResultMessage);
+                }
+
+                await Task.Delay(10000);
+            }
+        }
     }
 }
