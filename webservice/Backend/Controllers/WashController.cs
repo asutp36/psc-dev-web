4386622ab9ee3f8a0ecff06185a37471568b822f
neckbor <neckbor@mail.ru>
2021-06-23 19:47:42 +0300

    - backend: добавлена модель мойки для передчи на фронт; - заглушка метода выдачи всех моек;

---
+++ webservice/Backend/Controllers/WashController.cs
@@ -0,0 +1,45 @@
+﻿using Backend.Controllers.Supplies;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WashController : ControllerBase
+    {
+        private ModelDbContext _model;
+        private readonly ILogger<WashController> _logger;
+
+        public WashController(ILogger<WashController> logger)
+        {
+            _logger = logger;
+            _model = new ModelDbContext();
+        }
+
+        [SwaggerOperation(Summary = "Данные для страницы наличия в боксах по мойкам")]
+        [SwaggerResponse(200, Type = typeof(List<Wash>))]
+        [SwaggerResponse(500, Type = typeof(Error))]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<Wash> washes = new List<Wash>();
+
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, new Error(e.Message, "unexpected"));
+            }
+
+            return null;
+        }
+    }
+}
