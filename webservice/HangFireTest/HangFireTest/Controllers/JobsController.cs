d6702473f2a6685d01298b19f82ccf78fbcd660d
neckbor <neckbor@mail.ru>
2021-07-21 18:30:45 +0300

    - hangfire: модель и зашлушки методов для создания и запуска неотложной задачи;

--- webservice/HangFireTest/HangFireTest/Controllers/JobsController.cs
+++ webservice/HangFireTest/HangFireTest/Controllers/JobsController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HangFireTest.Controllers.Models;
 
 namespace HangFireTest.Controllers
 {
@@ -11,5 +13,30 @@ namespace HangFireTest.Controllers
     [ApiController]
     public class JobsController : ControllerBase
     {
+        #region Swagger Annotations
+        [SwaggerOperation(Summary = "Создать немедленную задачу")]
+        [SwaggerResponse(200, Description = "Задача создана")]
+        #endregion
+        [HttpPost("imidiate")]
+        public async Task<IActionResult> CreateImidiateJobAsync(WhattsAppReportImidateJobModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok();
+        }
+
+        #region Swagger Annotations
+        [SwaggerOperation(Summary = "Запустить немедленную задачу")]
+        [SwaggerResponse(200, Description = "Задача запущена")]
+        #endregion
+        [HttpGet("imidiate/{recipient}")]
+        public async Task<IActionResult> RunImidiateJobAsync(int recipient)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok();
+        }
     }
 }
---
+++ webservice/HangFireTest/HangFireTest/Controllers/Models/WhattsAppReportImidateJobModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFireTest.Controllers.Models
+{
+    public class WhattsAppReportImidateJobModel
+    {
+        public int recipient { get; set; }
+        public string chatId { get; set; }
+        public string washCode { get; set; }
+    }
+}
