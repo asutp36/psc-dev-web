c17058bc9429be5442a88e88ed201aba279d991c
neckbor <neckbor@mail.ru>
2021-07-21 14:42:19 +0300

    - hangfire: добавил модель результата хранимки и дополнительный контекст, где прописал хранимку;

---
+++ webservice/HangFireTest/HangFireTest/JobHelpers/Models/GetDayReportIncrease_Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFireTest.JobHelpers.Models
+{
+    public class GetDayReportIncrease_Result
+    {
+        public string msg { get; set; }
+    }
+}
---
+++ webservice/HangFireTest/HangFireTest/JobHelpers/Models/WashCompanyDbContext_Procedures.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFireTest.JobHelpers.Models
+{
+    public partial class WashCompanyDbContext
+    {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GetDayReportIncrease_Result>().HasNoKey();
+        }
+    }
+}
