06549aa87c1604c2b3252c66a5bcdd600c893149
neckbor <neckbor@mail.ru>
2021-08-17 16:49:05 +0300

    - hangfire: класс и контроллер для создания тестовых задач;

---
+++ webservice/HangFireTest/HangFireTest/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Hangfire;
+using Hangfire.Storage;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFireTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private IBackgroundJobClient _backgroundJobs;
+
+        public TestController(IBackgroundJobClient backgroundJobs)
+        {
+            _backgroundJobs = backgroundJobs;
+        }
+
+        [HttpGet("jobs/recurring")]
+        public IActionResult GetRecurringJob()
+        {
+            List<RecurringJobDto> recurringJobs = JobStorage.Current.GetConnection().GetRecurringJobs().ToList();
+
+            return Ok(recurringJobs);
+        }
+
+        [HttpPost("reccuring")]
+        public IActionResult ReccuringJob()
+        {
+            RecurringJob.AddOrUpdate($"", () => Console.WriteLine(""), "0 0 0 * * *");
+            return StatusCode(201);
+        }
+    }
+}
