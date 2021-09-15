45cfa8c29a9e628ece62907351c9ecd24b5b2775
neckbor <neckbor@mail.ru>
2021-09-15 14:41:52 +0300

    - postrc: добавил контроллер и модель скидок, написал метод получения скидок по мойке;

---
+++ webservice/PostControllingService/PostControllingService/Controllers/PostDiscountController.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using PostControllingService.Controllers.Supplies;
+using PostControllingService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PostControllingService.Controllers
+{
+    public class PostDiscountController : ApiController
+    {
+        ModelDb _model = new ModelDb();
+
+        [HttpGet]
+        public IHttpActionResult Get([FromUri]string washCode)
+        {
+            Logger.InitLogger();
+            try
+            {
+                if (washCode == null || washCode == "")
+                {
+                    Logger.Log.Error("PostDiscount.Get: нет кода мойки" + Environment.NewLine);
+                    return BadRequest();
+                }
+
+                List<PostDiscount> result = new List<PostDiscount>();
+                List<Posts> posts = _model.Posts.Where(p => p.IDWash == _model.Wash.Where(w => w.Code == washCode).FirstOrDefault().IDWash && !p.Code.Contains("V")).ToList();
+                foreach(Posts p in posts)
+                {
+                    string ip = GetPostIp(p.Code);
+                    if (ip == null || ip.Equals(""))
+                    {
+                        Logger.Log.Error("PostDiscount.Get: не найден ip адрес поста " + p.Code);
+                        continue;
+                    }
+
+                    HttpSenderResponse response = HttpSender.SendGet("http://" + ip + "api/post/get/happyhours");
+
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        Logger.Log.Error("PostDiscount.Get: " + String.Format("Ответ поста: {0}\n{1}", response.StatusCode, response.ResultMessage) + Environment.NewLine);
+                        continue;
+                    }
+
+                    PostDiscount discount = JsonConvert.DeserializeObject<PostDiscount>(response.ResultMessage);
+                    discount.Post = p.Code;
+                    result.Add(discount);
+                }
+
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                Logger.Log.Error("PostDiscount.Get: " + e.Message + Environment.NewLine + e.StackTrace + Environment.NewLine);
+                return InternalServerError();
+            }
+        }
+
+        private string GetPostIp(string code)
+        {
+            Device device = _model.Device.Where(d => d.Code.Equals(code)).FirstOrDefault();
+            if (device == null)
+            {
+                return null;
+            }
+
+            string ip = device.IpAddress;
+
+            if (ip.Length > 1)
+                return ip;
+
+            return null;
+        }
+    }
+}
---
+++ webservice/PostControllingService/PostControllingService/Controllers/Supplies/PostDiscount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostControllingService.Controllers.Supplies
+{
+    public class PostDiscount
+    {
+        public string Post { get; set; }
+        public DateTime HappyHourBeg { get; set; }
+        public DateTime HappyHourEnd { get; set; }
+        public int HappyHourSale { get; set; }
+    }
+}
