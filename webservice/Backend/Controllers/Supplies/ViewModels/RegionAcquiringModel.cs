dd6d4205416db689ddf9255603700d0dc8aa930d
neckbor <neckbor@mail.ru>
2021-10-06 13:35:27 +0300

    - backend: добавил модель настроек эквайринга по региону;

---
+++ webservice/Backend/Controllers/Supplies/ViewModels/RegionAcquiringModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers.Supplies.ViewModels
+{
+    public class RegionAcquiringModel
+    {
+        public int regionCode { get; set; }
+        public string regionName { get; set; }
+        public List<WashAcquiringViewModel> washes { get; set; }
+    }
+}
