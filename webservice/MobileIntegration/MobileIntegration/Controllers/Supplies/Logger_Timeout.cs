211fec48cf34e8926c7091013d2487ba68a1d420
neckbor <neckbor@mail.ru>
2021-10-12 15:41:57 +0300

    - mobile-int: добавил логгер времени ответов постов на запуск;

---
+++ webservice/MobileIntegration/MobileIntegration/Controllers/Supplies/Logger_Timeout.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace MobileIntegration.Controllers.Supplies
+{
+    public class Logger_Timeout
+    {
+        private static ILog log = LogManager.GetLogger("LOGGER_TIMEOUT");
+
+        public static ILog Log
+        {
+            get { return log; }
+        }
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
