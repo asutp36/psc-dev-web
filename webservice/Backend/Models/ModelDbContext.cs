c8cc0fb0e7a781e5ce2d660403f7893d41abd03b
neckbor <neckbor@mail.ru>
2021-10-22 16:43:17 +0300

    -backend: добавил модели;

--- webservice/Backend/Models/ModelDbContext.cs
+++ webservice/Backend/Models/ModelDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace Backend.Models
 {
--- webservice/Backend/Startup.cs
+++ webservice/Backend/Startup.cs
@@ -31,7 +31,6 @@ namespace Backend
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
@@ -72,7 +71,6 @@ namespace Backend
 
             services.AddControllers();
 
-            // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
@@ -106,22 +104,15 @@ namespace Backend
             services.AddMemoryCache();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env/*, ILoggerFactory loggerFactory*/)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("./swagger/v1/swagger.json", "mobile-int");
                 c.RoutePrefix = string.Empty;
             });
-
-            //loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "Logs/logger.txt"));
-            //var logger = loggerFactory.CreateLogger("FileLogger");
 
             if (env.IsDevelopment())
             {
