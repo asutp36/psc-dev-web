77438c4a375c9928e0d8506cd95701eef9eda995
neckbor <neckbor@mail.ru>
2021-10-25 21:25:05 +0300

    -postrc.core: добавил контроллер и модель для отправки и получения процента фискализации; - в HttpSender добавил метод отправки PATCH запроса;

---
+++ webservice/PostRCService/Controllers/BindingModels/FiscalPercentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostRCService.Controllers.BindingModels
+{
+    public class FiscalPercentModel
+    {
+        public int FiscalizationPercentage { get; set; }
+    }
+}
