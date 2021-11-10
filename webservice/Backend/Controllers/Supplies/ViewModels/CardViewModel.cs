c89f39bd7eca8701cf6f1b631ee0a84e146222be
neckbor <neckbor@mail.ru>
2021-11-10 20:42:04 +0300

    - backend: коммит модели технической карты;

---
+++ webservice/Backend/Controllers/Supplies/ViewModels/CardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers.Supplies.ViewModels
+{
+    public class CardViewModel
+    {
+        public string num { get; set; }
+        public string type { get; set; }
+    }
+}
