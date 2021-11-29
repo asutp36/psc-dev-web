858f117191c17688925556c6c77f753dda6d6477
neckbor <neckbor@mail.ru>
2021-11-29 14:52:05 +0300

    - backend: добавл переопределние Equals;

--- webservice/Backend/Controllers/Supplies/ViewModels/FiscalPercentModel.cs
+++ webservice/Backend/Controllers/Supplies/ViewModels/FiscalPercentModel.cs
@@ -5,8 +5,21 @@ using System.Threading.Tasks;
 
 namespace Backend.Controllers.Supplies.ViewModels
 {
-    public class FiscalPercentModel
+    public class FiscalPercentModel : IEquatable<FiscalPercentModel>
     {
         public int FiscalizationPercentage { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is FiscalPercentModel && Equals(obj as FiscalPercentModel);
+        }
+
+        public bool Equals(FiscalPercentModel other)
+        {
+            if (other == null)
+                return false;
+
+            return this.FiscalizationPercentage == other.FiscalizationPercentage;
+        }
     }
 }
