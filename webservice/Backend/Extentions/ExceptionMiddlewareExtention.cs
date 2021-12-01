a16a3e0b6f32dd1cfa98f795d59e294523c33b54
neckbor <neckbor@mail.ru>
2021-12-01 12:56:07 +0300

    - backend: добавил класс, который будет встроен для глобального перехвата ошибок;

---
+++ webservice/Backend/Extentions/ExceptionMiddlewareExtention.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Backend.Controllers.Supplies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Backend.Extentions
+{
+    public static class ExceptionMiddlewareExtention
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILogger logger)
+        {
+            app.UseExceptionHandler(appError => 
+                {
+                    appError.Run(async context => 
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            context.Response.ContentType = "application/json";
+
+                            var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                            if(contextFeature != null)
+                            {
+                                logger.LogError("" + Environment.NewLine);
+
+                                await context.Response.WriteAsync(JsonConvert.SerializeObject(
+                                    new Error("Что-то пошло не так в ходе работы программы сервера. Обратитесь к специалисту.", "unexpected"))
+                                );
+                            }
+                        });
+                }
+            );
+        }
+    }
+}
