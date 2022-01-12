01eced5fba76c362a6c12dda2c05a41a0135cd80
neckbor <neckbor@mail.ru>
2022-01-12 13:27:49 +0300

    - postsync.core: новые модели;

---
+++ webservice/PostSyncService/Models/GateWash/DeviceTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace PostSyncService.Models.GateWash
+{
+    public partial class DeviceTypes
+    {
+        public DeviceTypes()
+        {
+            Device = new HashSet<Device>();
+        }
+
+        public int IddeviceType { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Device> Device { get; set; }
+    }
+}
---
+++ webservice/PostSyncService/Models/GateWash/Sessions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace PostSyncService.Models.GateWash
+{
+    public partial class Sessions
+    {
+        public Sessions()
+        {
+            Event = new HashSet<Event>();
+        }
+
+        public int Idsession { get; set; }
+        public int IdsessoinOnWash { get; set; }
+        public int Iddevice { get; set; }
+        public int Idfunction { get; set; }
+        public string Idcard { get; set; }
+        public DateTime Dtime { get; set; }
+        public string Uuid { get; set; }
+
+        public virtual ICollection<Event> Event { get; set; }
+    }
+}
