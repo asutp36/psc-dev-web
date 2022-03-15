36b023f211cbf74794d97691788e72cd6defa148
neckbor <neckbor@mail.ru>
2022-03-15 13:52:53 +0300

    - gatewashdata: добавил сваггер;

--- webservice/GateWashDataService/Startup.cs
+++ webservice/GateWashDataService/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,11 +32,31 @@ namespace GateWashDataService
 
             services.AddDbContext<GateWashDbContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("GateWash")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "gatewashdata",
+                    Description = "—ервис получени€ данных дл€ проходных моек",
+                });
+
+                c.EnableAnnotations();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("./swagger/v1/swagger.json", "gatewashdata");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
