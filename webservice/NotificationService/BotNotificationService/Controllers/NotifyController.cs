adf8d691dbed8d82eb91b366e500e04344c2bf0e
neckbor <neckbor@mail.ru>
2022-05-24 14:36:51 +0300

    - bot notify: добавил модель кредов бота, тестовый вызов;

--- webservice/NotificationService/BotNotificationService/Controllers/NotifyController.cs
+++ webservice/NotificationService/BotNotificationService/Controllers/NotifyController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BotNotificationService.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BotNotificationService.Controllers
@@ -11,5 +14,40 @@ namespace BotNotificationService.Controllers
     [ApiController]
     public class NotifyController : ControllerBase
     {
+        private readonly ILogger<NotifyController> _logger;
+        public NotifyController(ILogger<NotifyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetInfoAsync()
+        {
+            var response = await SendRequestAsync();
+            string data = await response.Content.ReadAsStringAsync();
+            return Ok(data);
+        }
+
+        private async Task<HttpResponseMessage> SendRequestAsync()
+        {
+            try
+            {
+                BotCredentials bot = new BotCredentials();
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri($"https://api.telegram.org/bot{bot.Token}/");
+
+                HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, "getMe");
+
+                var result = await client.SendAsync(message);
+
+                return result;
+            }
+            catch(HttpRequestException e)
+            {
+                HttpResponseMessage failedMessage = new HttpResponseMessage();
+
+                return null;
+            }
+        }
     }
 }
---
+++ webservice/NotificationService/BotNotificationService/Models/BotCredentials.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotNotificationService.Models
+{
+    public class BotCredentials
+    {
+        public string Token = "5329979997:AAF3sHB7D2k6pFCxED6U4OduvtIXdVoDy8s";
+    }
+}
