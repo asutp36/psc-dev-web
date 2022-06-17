4f1210a394a01b070da6f43025c46d06ad88b39a
neckbor <neckbor@mail.ru>
2022-06-17 15:04:57 +0300

    - mangoAPI: добавил класс для отправки http запросов;

---
+++ webservice/MangoAPIService/Helpers/HttpSender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MangoAPIService.Helpers
+{
+    public class HttpSender
+    {
+        private HttpClient _httpClient;
+
+        public HttpSender(string baseAddress)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(baseAddress);
+        }
+
+        public async Task<HttpResponseMessage> Post(string url, string content)
+        {
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, url);
+
+            if(content != null)
+            {
+                requestMessage.Content = new StringContent(content, Encoding.UTF8, Application.Json);
+            }
+
+            HttpResponseMessage result = await _httpClient.SendAsync(requestMessage);
+
+            return result;
+        }
+    }
+}
