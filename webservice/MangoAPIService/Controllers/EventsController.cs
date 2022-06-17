e2bb2624a6cbdfe7b8e911f3bf4049da0ef1c272
neckbor <neckbor@mail.ru>
2022-06-17 12:50:03 +0300

    - mangoAPI: добавил класс для работы с хэшем; - в конструкторе контроллера беру значение api salt из конфига; - добавил класс параметров запроса от MnagoAPI; - добавил проверку подписи в из запроса;

--- webservice/MangoAPIService/Controllers/EventsController.cs
+++ webservice/MangoAPIService/Controllers/EventsController.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MangoAPIService.Models;
+using MangoAPIService.Helpers;
+using System.Security.Cryptography;
+using Microsoft.Extensions.Configuration;
 
 namespace MangoAPIService.Controllers
 {
@@ -13,15 +17,23 @@ namespace MangoAPIService.Controllers
     public class EventsController : ControllerBase
     {
         public readonly ILogger<EventsController> _logger;
+        public readonly string _apiSalt;
 
-        public EventsController(ILogger<EventsController> logger)
+        public EventsController(ILogger<EventsController> logger, IConfiguration config)
         {
             _logger = logger;
+            _apiSalt = config.GetValue<string>("ApiSalt", null);
         }
 
         [HttpPost("call")]
-        public async Task<IActionResult> Call()
+        public async Task<IActionResult> Call([FromBody] MangoAPIRequestParameters parameters)
         {
+            if(!Hasher.VerifyHash(HashAlgorithm.Create("SHA256"), parameters.sign, parameters.vpbx_api_key + parameters.json + _apiSalt))
+            {
+                _logger.LogError("Подпись не прошла проверку" + Environment.NewLine);
+                return Unauthorized();
+            }
+
             return Ok();
         }
     }
---
+++ webservice/MangoAPIService/Models/MangoAPIRequestParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangoAPIService.Models
+{
+    public class MangoAPIRequestParameters
+    {
+        public string vpbx_api_key { get; set; }
+        public string sign { get; set; }
+        public string json { get; set; }
+    }
+}
