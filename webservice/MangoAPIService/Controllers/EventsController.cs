41923d3c508c95529aab3782f78ff9ccb01dc29f
neckbor <neckbor@mail.ru>
2022-06-17 11:41:34 +0300

    - mangoAPI: добавил контроллер для обработки событий;

---
+++ webservice/MangoAPIService/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MangoAPIService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        public readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("call")]
+        public async Task<IActionResult> Call()
+        {
+            return Ok();
+        }
+    }
+}
