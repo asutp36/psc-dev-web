4fe016be3fd38c00a608e0f71d0d36321f7a18f0
neckbor <neckbor@mail.ru>
2022-07-04 13:10:20 +0300

    - loyality: добавил новый проект со сваггером, логгером, ef core;

---
+++ webservice/LoyalityService/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoyalityService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            // Get the factory for ILogger instances.
+            var nlogLoggerProvider = new NLogLoggerProvider();
+
+            // Create an ILogger.
+            Logger = nlogLoggerProvider.CreateLogger(typeof(Startup).FullName);
+        }
+
+        public IConfiguration Configuration { get; }
+        public ILogger Logger { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "LoyalityService",
+                    Description = "Сервис для высчитывания скидки",
+                    Version = "v1"
+                });
+
+                s.EnableAnnotations();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("./swagger/v1/swagger.json", "LoyalityService");
+                s.RoutePrefix = string.Empty;
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
