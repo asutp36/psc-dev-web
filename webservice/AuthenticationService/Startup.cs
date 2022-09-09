9a5c15cbe9ebb1968b564d6fd5f5af1943c1eadc
neckbor <neckbor@mail.ru>
2022-09-09 15:36:40 +0300

    - auth: подключил сваггер;

--- webservice/AuthenticationService/Startup.cs
+++ webservice/AuthenticationService/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,11 +27,31 @@ namespace AuthenticationService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "AuthenticationService",
+                    Description = "Сервис аутентификации",
+                    Version = "v1"
+                });
+
+                s.EnableAnnotations();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("./swagger/v1/swagger.json", "AuthenticationService");
+                s.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
