8487e2ec18b04d7bc82db29bd566351b68d3aaba
neckbor <neckbor@mail.ru>
2022-09-13 12:57:59 +0300

    - auth: добавил модель бд;

---
+++ webservice/AuthenticationService/Models/UserAuthenticationDb/UserAuthenticationDbContext.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace AuthenticationService.Models.UserAuthenticationDb
+{
+    public partial class UserAuthenticationDbContext : DbContext
+    {
+        public UserAuthenticationDbContext()
+        {
+        }
+
+        public UserAuthenticationDbContext(DbContextOptions<UserAuthenticationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserWash> UserWashes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=UserAuthentication;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.HasKey(e => e.Idrole)
+                    .HasName("PK_Role");
+
+                entity.Property(e => e.Idrole).HasColumnName("IDRole");
+
+                entity.Property(e => e.Code)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Iduser);
+
+                entity.Property(e => e.Iduser).HasColumnName("IDUser");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Idrole).HasColumnName("IDRole");
+
+                entity.Property(e => e.Login)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Phone).HasMaxLength(20);
+
+                entity.HasOne(d => d.IdroleNavigation)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.Idrole)
+                    .HasConstraintName("FK_Users_Roles");
+            });
+
+            modelBuilder.Entity<UserWash>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("UserWash");
+
+                entity.Property(e => e.Iduser).HasColumnName("IDUser");
+
+                entity.Property(e => e.WashCode)
+                    .IsRequired()
+                    .HasMaxLength(10);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ webservice/AuthenticationService/Models/UserAuthenticationDb/UserWash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AuthenticationService.Models.UserAuthenticationDb
+{
+    public partial class UserWash
+    {
+        public int Iduser { get; set; }
+        public string WashCode { get; set; }
+    }
+}
