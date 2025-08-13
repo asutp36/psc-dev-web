f29ec07ee4aa2f3d32d02e775ef51c63a7bb51c7
neckbor <neckbor@mail.ru>
2025-08-13 13:24:43 +0400

    - заменём параметр time_send с DateTime на string;

--- webservice/MobileIntegration/MobileIntegration/Controllers/Supplies/StartPostBindingModel.cs
+++ webservice/MobileIntegration/MobileIntegration/Controllers/Supplies/StartPostBindingModel.cs
@@ -7,7 +7,7 @@ namespace MobileIntegration.Controllers.Supplies
 {
     public class StartPostBindingModel
     {
-        public DateTime time_send { get; set; }
+        public string time_send { get; set; }
         public string hash { get; set; }
         public string card { get; set; }
         public string post { get; set; }
