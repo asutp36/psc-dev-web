9dd6fefba8aed853b2b688802767f40a6bee9cc3
neckbor <neckbor@mail.ru>
2025-08-13 13:45:59 +0400

    - Telegram bot options модель и биндинг из конфигурации;

---
+++ webservice/NotificationService/BotNotificationService/Options/TelegramBotOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BotNotificationService.Options
+{
+    public class TelegramBotOptions
+    {
+        public string Token { get; set; }
+        public List<long> AcceptableCommandUsers { get; set; }
+    }
+}
---
+++ webservice/NotificationService/BotNotificationService/Options/TelegramBotOptionsSetup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace BotNotificationService.Options
+{
+    public class TelegramBotOptionsSetup : IConfigureOptions<TelegramBotOptions>
+    {
+        private const string SectionName = "BotOptions";
+        
+        private readonly IConfiguration _configuration;
+
+        public TelegramBotOptionsSetup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(TelegramBotOptions options)
+        {
+            _configuration.GetSection(SectionName).Bind(options);
+        }
+    }
+}
